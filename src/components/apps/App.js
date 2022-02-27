277b690d6d4f5f1e3e83278088940c1b54759a57
Shaindy Koster <shaindykoster@gmail.com>
2022-02-26 20:39:23 -0500

    created an app header

--- src/components/apps/App.js
+++ src/components/apps/App.js
@@ -1,11 +1,26 @@
 import {Home} from '../home/home';
 import {Todo} from '../todo/todo';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import {Header} from '../header/header'
 
 function App(){
   return(
     <div className='App'>
-     {/* <Home></Home>*/}
-      <Todo/>
+      
+
+
+  <BrowserRouter>
+  <Header/>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+        <Route path="/todo" element={<Todo/>}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   )
 }
